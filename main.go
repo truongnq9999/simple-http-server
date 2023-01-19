c133d38d29f94307a2e037deb306b83118565c4e
truongnq1 <truongnq1@vnpay.vn>
2023-01-19 11:11:16 +0700

    feat: return request data

--- main.go
+++ main.go
@@ -20,19 +20,28 @@ func main() {
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Request:", r.Method, r.URL.Path)
+
+		responseBody := ""
+
 		if *showHeader {
 			log.Println("=====================header=====================")
+			responseBody += "=====================header=====================" + "\n"
 			for k, v := range r.Header {
 				log.Println("==>", k, ": ", v)
+				responseBody += k + ": " + v[0] + "\n"
 			}
+			responseBody += "====================end header===================" + "\n"
 			log.Println("====================end header===================")
 		}
 
 		query := r.URL.Query()
 		log.Println("=====================query=====================")
+		responseBody += "=====================query=====================" + "\n"
 		for k, v := range query {
 			log.Println("==>", k, ": ", v)
+			responseBody += k + ": " + v[0] + "\n"
 		}
+		responseBody += "====================end query===================" + "\n"
 		log.Println("===================end query======================")
 
 		// Read body to string
@@ -45,13 +54,17 @@ func main() {
 		}
 
 		log.Println("=====================body=====================")
+		responseBody += "=====================body=====================" + "\n"
 		log.Println("==>", string(body))
+		responseBody += string(body) + "\n"
+		responseBody += "====================end body===================" + "\n"
 		log.Println("===================end body=====================")
-		if len(body) > 0 {
-			_, err = w.Write(body)
-		} else {
-			_, err = w.Write([]byte(query.Encode()))
+
+		_, err = w.Write([]byte(responseBody))
+		if err != nil {
+			return
 		}
+
 		if err != nil {
 			log.Printf("error: %v", err)
 			return
