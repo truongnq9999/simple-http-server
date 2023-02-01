312a743581238243171ab81024b7edee1d506060
truongnq1 <truongnq1@vnpay.vn>
2023-02-01 14:18:46 +0700

    fix: dump request

--- main.go
+++ main.go
@@ -4,65 +4,32 @@ package main
 
 import (
 	"flag"
-	"io"
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/httputil"
 )
 
 func main() {
 
 	port := flag.String("port", "8080", "port to listen on")
-	showHeader := flag.Bool("show-header", true, "show header")
+	//showHeader := flag.Bool("show-header", true, "show header")
 
 	flag.Parse()
 
 	log.Println("Args:", flag.Args())
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Request:", r.Method, r.URL.Path)
-
-		responseBody := ""
-
-		if *showHeader {
-			log.Println("=====================header=====================")
-			responseBody += "=====================header=====================" + "\n"
-			for k, v := range r.Header {
-				log.Println("==>", k, ": ", v)
-				responseBody += k + ": " + v[0] + "\n"
-			}
-			responseBody += "====================end header===================" + "\n"
-			log.Println("====================end header===================")
-		}
-
-		query := r.URL.Query()
-		log.Println("=====================query=====================")
-		responseBody += "=====================query=====================" + "\n"
-		for k, v := range query {
-			log.Println("==>", k, ": ", v)
-			responseBody += k + ": " + v[0] + "\n"
-		}
-		responseBody += "====================end query===================" + "\n"
-		log.Println("===================end query======================")
-
-		// Read body to string
-		body, err := io.ReadAll(r.Body)
-
+		reqDump, err := httputil.DumpRequest(r, true)
 		if err != nil {
-			log.Printf("error reading body: %v", err)
-			http.Error(w, "error reading body", http.StatusInternalServerError)
-			return
+			log.Fatal(err)
 		}
 
-		log.Println("=====================body=====================")
-		responseBody += "=====================body=====================" + "\n"
-		log.Println("==>", string(body))
-		responseBody += string(body) + "\n"
-		responseBody += "====================end body===================" + "\n"
-		log.Println("===================end body=====================")
+		fmt.Printf("REQUEST:\n%s", string(reqDump))
 
 		// Write response
 		w.Header().Set("Content-Type", "text/plain")
-		_, err = w.Write([]byte(responseBody))
+		_, err = w.Write(reqDump)
 		if err != nil {
 			return
 		}
